7e2a2179998ddbd3095c0d79fb39023a842378ec
dimalvovs <dmitrijs.lvovs@gmail.com>
2025-04-21 21:57:11 -0400

    adjust for presence/absence of raw.X in anndata

--- modules/local/rctd/templates/rctd.r
+++ modules/local/rctd/templates/rctd.r
@@ -14,37 +14,54 @@ outdir <- '${prefix}'
 process <- '${task.process}'
 cell_type_col <- '${params.type_col_scrna}'
 
-message('prepare rctd ref object')
-#1. counts - genes need to be in rows, cells in columns
-adata_sc <- ad[['read_h5ad']](adata_sc_path)
-counts_sc <- Matrix::t(as(adata_sc[['X']], "CsparseMatrix"))
-rownames(counts_sc) <- as.character(adata_sc[['var_names']][['values']])
-colnames(counts_sc) <- as.character(adata_sc[['obs_names']][['values']])
+#!/usr/bin/env Rscript
+library(reticulate)
+ad <- reticulate::import("anndata")
 
-#2. celltypes must be named factor
-message("found columns: ", paste(colnames(adata_sc[['obs']]), collapse = ', '))
-if(!cell_type_col %in% colnames(adata_sc[['obs']])) {
-  stop(sprintf('cell type column "%s" not found in adata_sc', cell_type_col))
-}
-celltypes_sc <- as.character(adata_sc[['obs']][[cell_type_col]])
-names(celltypes_sc) <- adata_sc[['obs_names']][['values']]
-celltypes_sc <- as.factor(celltypes_sc)
-
-#3. create reference object
-ref <- spacexr::Reference(cell_types=celltypes_sc, counts=counts_sc)
-
-message('prepare query object')
-adata_st <- ad[['read_h5ad']](adata_st_path)
+### prep spatial (query) object
 #1. coords need to be present in anndata.obsm['spatial']
-spatial <- as.data.frame(adata_st[['obsm']]['spatial'])
-rownames(spatial) <- adata_st[['obs_names']][['values']]
+adata_st <- ad[["read_h5ad"]](adata_st_path)
+spatial <- as.data.frame(adata_st[["obsm"]]["spatial"])
+rownames(spatial) <- adata_st[["obs_names"]][["values"]]
 #2. counts - genes need to be in rows, cells in columns
 counts_st <- Matrix::t(as(adata_st[['X']], "CsparseMatrix"))
 rownames(counts_st) <- as.character(adata_st[['var_names']][['values']])
 colnames(counts_st) <- as.character(adata_st[['obs_names']][['values']])
 query <- spacexr::SpatialRNA(coords=spatial, counts=counts_st)
 
-message('run rctd')
+
+### prep reference object
+#1. counts - genes need to be in rows, cells in columns, opposite to anndata
+adata_sc <- ad[["read_h5ad"]](adata_sc_path, backed = "r")
+
+if (is.null(adata_sc[["raw"]])) {
+  counts_sc <- Matrix::t(adata_sc[["X"]][])
+  rownames(counts_sc) <- rownames(adata_sc[["var"]])
+} else {
+  counts_sc <- Matrix::t(adata_sc[["raw"]][["X"]][])
+  rownames(counts_sc) <- rownames(adata_sc[["raw"]][["var"]])
+}
+
+colnames(counts_sc) <- as.character(adata_sc[["obs_names"]][["values"]])
+
+#2. celltypes must be named factor
+obs_cols <- colnames(adata_sc[['obs']])
+message("found columns: ", paste(obs_cols, collapse = ', '))
+if(!cell_type_col %in% obs_cols) {
+  stop(sprintf('cell type column "%s" not found in adata_sc', cell_type_col))
+}
+celltypes_sc <- as.character(adata_sc[['obs']][[cell_type_col]])
+names(celltypes_sc) <- adata_sc[['obs_names']][['values']]
+celltypes_sc <- as.factor(celltypes_sc)
+
+#3. drop non-matching genes from atlas object & convert to column orientation
+counts_sc <- counts_sc[rownames(counts_sc) %in% rownames(counts_st), ]
+
+#4. create reference object
+ref <- spacexr::Reference(cell_types=celltypes_sc, counts=counts_sc)
+
+message('run rctd') 
+#this converts the reference object to a dense matrix
 rctd <- spacexr::create.RCTD(spatialRNA=query, reference=ref, max_cores = ncores)
 rctd_res <- spacexr::run.RCTD(rctd, doublet_mode = 'full')
 
