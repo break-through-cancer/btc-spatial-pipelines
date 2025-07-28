cfd28cd9d05705221c8b6b1e022d4068ef8b8d25
dimalvovs <dmitrijs.lvovs@gmail.com>
2025-07-28 16:41:14 -0400

    wrap into name/main

--- modules/local/squidpy/templates/ligrec.py
+++ modules/local/squidpy/templates/ligrec.py
@@ -7,42 +7,8 @@ import os
 import pickle
 from matplotlib import pyplot as plt
 
-logging.basicConfig(level=logging.INFO)
-log = logging.getLogger()
 
-
-adata_path = "${adata}"
-sample = "${prefix}"
-process = "${task.process}"
-cpus = ${task.cpus}
-
-par = {"sq_gr_ligrec_threshold": ${params.sq_gr_ligrec_threshold},
-       "sq_gr_ligrec_alpha": ${params.sq_gr_ligrec_alpha},
-       "sq_gr_ligrec_nperms": ${params.sq_gr_ligrec_nperms},
-       "sq_pl_ligrec_pvalue": ${params.sq_pl_ligrec_pvalue}}
-
-log.info("received params:{}".format(par))
-
-log.info("loading {}".format(adata_path))
-adata = ad.read_h5ad(adata_path)
-log.info("adata is {}".format(adata))
-
-#squidpy insists on figure dir naming, not creating plot outdir as usually
-base_path = os.getcwd()
-save_path = os.path.join(sample, "ligrec")
-os.makedirs(save_path, exist_ok=True)
-os.chdir(save_path)
-
-#filter non-na cell_types
-adata = adata[~adata.obs["cell_type"].isna()].copy()
-clusters = adata.obs["cell_type"].unique()
-
-#normalize and log1p
-log.info("normalizing and log1p transforming data")
-sc.pp.normalize_total(adata)
-sc.pp.log1p(adata)
-
-def default_ligrec(**kwargs):
+def default_ligrec(adata, **kwargs):
     if "gene_symbols" in kwargs:
         gene_symbols = kwargs.pop("gene_symbols")
     else:
@@ -60,17 +26,16 @@ def default_ligrec(**kwargs):
         alpha=par["sq_gr_ligrec_alpha"],
         gene_symbols=gene_symbols,
         threshold=par["sq_gr_ligrec_threshold"],
-        numba_parallel=False
-        
-    )
+        numba_parallel=False)
+    
     return ligrec
 
-def default_ligrec_pl(**kwargs): 
+def default_ligrec_pl(ligrec, **kwargs): 
     source_groups = kwargs.pop("source_groups")
     target_groups = kwargs.pop("target_groups")
     save = kwargs.pop("save")
 
-    sq.pl.ligrec(res,
+    sq.pl.ligrec(ligrec,
             source_groups=source_groups,
             target_groups=target_groups,
             save=save,
@@ -79,55 +44,88 @@ def default_ligrec_pl(**kwargs):
             pvalue_threshold=par["sq_pl_ligrec_pvalue"],
             remove_nonsig_interactions=True,
             swap_axes=False)
+    plt.close('all')
+    return None
     
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger()
 
-# try running ligrec
-res = None
-try:
-    res = default_ligrec()
-except Exception as e:
-    log.error("default ligrec failed: {}".format(e))
+
+    adata_path = "${adata}"
+    sample = "${prefix}"
+    process = "${task.process}"
+    cpus = ${task.cpus}
+
+    par = {"sq_gr_ligrec_threshold": ${params.sq_gr_ligrec_threshold},
+        "sq_gr_ligrec_alpha": ${params.sq_gr_ligrec_alpha},
+        "sq_gr_ligrec_nperms": ${params.sq_gr_ligrec_nperms},
+        "sq_pl_ligrec_pvalue": ${params.sq_pl_ligrec_pvalue}}
+
+    log.info("received params:{}".format(par))
+
+    log.info("loading {}".format(adata_path))
+    adata = ad.read_h5ad(adata_path)
+    log.info("adata is {}".format(adata))
+
+    #squidpy insists on figure dir naming, not creating plot outdir as usually
+    base_path = os.getcwd()
+    save_path = os.path.join(sample, "ligrec")
+    os.makedirs(save_path, exist_ok=True)
+    os.chdir(save_path)
+
+    #filter non-na cell_types
+    adata = adata[~adata.obs["cell_type"].isna()].copy()
+    clusters = adata.obs["cell_type"].unique()
+
+    #normalize and log1p
+    log.info("normalizing and log1p transforming data")
+    sc.pp.normalize_total(adata)
+    sc.pp.log1p(adata)
+    # try running ligrec
+    res = None
     try:
-        res = default_ligrec(gene_symbols="index")
-    except Exception as e2:
-        log.error("ligrec without gene_symbols failed: {}".format(e2))
-
-if res is None:
-    log.error("ligrec did not return results.")
-else:
-    log.info("ligres completed successfully, saving to pickle")
-    # dictionary of pandas frames: means, pvalues, metadata
-    pickle.dump(res, open("ligrec_interactions.pickle", "wb"))
-
-    log.info("saving ligrec metadata")
-    # just metadata, as it's easy to comprehend, one row per interaction
-    res["metadata"].to_csv(
-        "ligrec_metadata.csv"
-    )
-
-    log.info("saving ligrec interaction plot")
-    for s in clusters:
+        res = default_ligrec()
+    except Exception as e:
+        log.error("default ligrec failed: {}".format(e))
         try:
-            log.info("plotting ligrec for source cluster {}".format(s))
-            default_ligrec_pl(source_groups=s, target_groups=clusters, save="source_{}.png".format(s))
-            plt.close('all')
-        except Exception as e:
-            log.error("ligrec plot for source {} failed: {}".format(s, e))
-            continue
-    
-    for t in clusters:
-        try:
-            log.info("plotting ligrec for target cluster {}".format(t))
-            default_ligrec_pl(source_groups=clusters, target_groups=t, save="target_{}.png".format(t))
-            plt.close('all')
-        except Exception as e:
-            log.error("ligrec plot for target {} failed: {}".format(t, e))
-            continue
+            res = default_ligrec(gene_symbols="index")
+        except Exception as e2:
+            log.error("ligrec without gene_symbols failed: {}".format(e2))
 
-os.chdir(base_path)
-with open ("versions.yml", "w") as f:
-    f.write("{}:\\n".format(process))
-    f.write("    squidpy: {}\\n".format(sq.__version__))
-    f.write("    anndata: {}\\n".format(ad.__version__))
-    f.write("    scanpy: {}\\n".format(sc.__version__))
+    if res is None:
+        log.error("ligrec did not return results.")
+    else:
+        log.info("ligres completed successfully, saving to pickle")
+        # dictionary of pandas frames: means, pvalues, metadata
+        pickle.dump(res, open("ligrec_interactions.pickle", "wb"))
 
+        log.info("saving ligrec metadata")
+        # just metadata, as it's easy to comprehend, one row per interaction
+        res["metadata"].to_csv(
+            "ligrec_metadata.csv"
+        )
+
+        log.info("saving ligrec interaction plot")
+        for s in clusters:
+            try:
+                log.info("plotting ligrec for source cluster {}".format(s))
+                default_ligrec_pl(source_groups=s, target_groups=clusters, save="source_{}.png".format(s))
+            except Exception as e:
+                log.error("ligrec plot for source {} failed: {}".format(s, e))
+                continue
+        
+        for t in clusters:
+            try:
+                log.info("plotting ligrec for target cluster {}".format(t))
+                default_ligrec_pl(source_groups=clusters, target_groups=t, save="target_{}.png".format(t))
+            except Exception as e:
+                log.error("ligrec plot for target {} failed: {}".format(t, e))
+                continue
+
+    os.chdir(base_path)
+    with open ("versions.yml", "w") as f:
+        f.write("{}:\\n".format(process))
+        f.write("    squidpy: {}\\n".format(sq.__version__))
+        f.write("    anndata: {}\\n".format(ad.__version__))
+        f.write("    scanpy: {}\\n".format(sc.__version__))
