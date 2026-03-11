881cbdf1a2030bd65b47725ed1f880b70207d23c
dimalvovs <dmitrijs.lvovs@gmail.com>
2026-03-11 09:14:51 -0400

    add missing template

---
+++ modules/local/util/templates/adata_preprocess.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import anndata as ad
+import logging
+import os
+
+drop_prefix = "${params.drop_genes_prefix}"
+adata_path = "${adata}"
+sample = "${meta.id}"
+
+def adata_preprocess(adata, drop_prefix):
+    if "drop_genes_prefix" not in drop_prefix:
+        adata = adata[:, ~adata.var_names.str.startswith(drop_prefix)]
+    adata.file.close()
+    return adata
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    log = logging.getLogger()
+    #output directory
+    os.makedirs(sample, exist_ok=True)
+    
+    adata = ad.read_h5ad(adata_path)
+    log.info(f"Before preprocessing adata: {adata}")
+
+    #preprocess adata
+    adata = adata_preprocess(adata, drop_prefix)
+    
+    #save
+    log.info(f"After preprocessing adata: {adata}")
+    adata.write_h5ad(f"{sample}/adata.h5ad")
+    
+    
+    #versions
+    with open("versions.yml", "w") as f:
+        f.write("${task.process}:\\n")
+        f.write("    anndata: {}\\n".format(ad.__version__))
